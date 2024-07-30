543aac45e45b38db4ce7548400bd1c64f0a9cf12
Stelios Serghiou <stelios.serghiou@gmail.com>
2024-07-29 18:57:14 -0700

    FEAT: Analyze all outcomes from one Rmd file

---
+++ code/draft-analyses/config_is-obliterated.R
@@ -0,0 +1,8 @@
+
+# Analysis name
+ANALYSIS_NAME <- "predictive-analytics/is-obliterated"
+
+# Outcome of choice
+CHOSEN_OUTCOME <- "is_obliterated"
+CHOSEN_OUTCOME_LEVELS <- c(FALSE, TRUE)
+CHOSEN_OUTCOME_LABELS <- c("Not obliterated", "Obliterated")
---
+++ code/draft-analyses/config_is-poor-outcome_haemTRUE.R
@@ -0,0 +1,8 @@
+
+# Analysis name
+ANALYSIS_NAME <- "predictive-analytics/poor-outcomes_has-hemorrhageTRUE"
+
+# Outcome of choice
+CHOSEN_OUTCOME <- "is_poor_outcome"
+CHOSEN_OUTCOME_LEVELS <- c(FALSE, TRUE)
+CHOSEN_OUTCOME_LABELS <- c("Good outcome", "Poor outcome")
