0783101cb5d1fcd4a7d5997e0a2fadf31025bdc7
Stelios Serghiou <stelios.serghiou@gmail.com>
2024-10-15 08:33:18 -0700

    BUGFIX: Change minor to major complications

--- code/draft-analyses/config_has-complication-major.R
+++ code/draft-analyses/config_has-complication-major.R
@@ -1,8 +1,8 @@
 
 # Analysis name
-ANALYSIS_NAME <- "predictive-analytics/complications-minor"
+ANALYSIS_NAME <- "predictive-analytics/complications-major"
 
 # Outcome definition
-CHOSEN_OUTCOME <- "has_complication_minor"
+CHOSEN_OUTCOME <- "has_complication_major"
 CHOSEN_OUTCOME_LEVELS <- c(FALSE, TRUE)
-CHOSEN_OUTCOME_LABELS <- c("No minor complications", "Minor complications")
+CHOSEN_OUTCOME_LABELS <- c("No major complications", "Major complications")
